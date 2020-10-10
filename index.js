568c38084a9e034d33745f30d462d4fe3d0729db
Elijah B <69051669+elijah415hz@users.noreply.github.com>
2020-10-09 20:27:48 -0700

    All basic functionality implemented

--- index.js
+++ index.js
@@ -1,16 +1,145 @@
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+const queries = require("./js/queries");
+
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "employee_tracker"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    init();
+    // console.log("connected as id " + connection.threadId + "\n");
+});
+
 //TODO: Inquirer Questions - Do with Async/Await
-    // View all Employees *
-    // View Employees by department *
-    // View Employees by roles *
-    // Add Employee *?
-    // Add Department *
-    // Add role *
-    // Bonus:
-    // Update Manager by employee *
-    // View employees by manager 
-    // Delete department 
-    // Delete role
-    // Delete employee
-    // View total salary by department
+// View all Employees *
+// View Employees by department *
+// View Employees by roles *
+// Add Employee *?
+// Add Department *
+// Add role *
+// Bonus:
+// Update Manager by employee *
+// View employees by manager 
+// Delete department 
+// Delete role
+// Delete employee
+// View total salary by department
+
 
 //TODO: Create queries for each question. How to store them?
+
+async function init() {
+    const answers = await inquirer.prompt({
+        type: "list",
+        message: "What would you like to do?",
+        name: "initQuestion",
+        choices: [
+            "View all Employees",
+            "View Employees by Department",
+            "View Employees by Role",
+            "Add Employee",
+            "Quit"
+        ]
+    })
+    switch (answers.initQuestion) {
+        case "View all Employees":
+            viewAllEmployees();
+            break;
+        case "View Employees by Department":
+            employeesByDepartment();
+            break;
+        case "View Employees by Role":
+            employeesByRole();
+            break;
+        case "Add Employee":
+            addEmployee();
+            break;
+        case "Quit":
+            connection.end();
+    }
+}
+
+async function viewAllEmployees() {
+    const employeesTable = await queries.viewEmployees.runQuery(connection)
+    console.table(employeesTable);
+        setTimeout(() => init(), 500);
+}
+
+async function employeesByDepartment() {
+    const departmentTable = await queries.viewDepartments.runQuery(connection);
+    let choices = departmentTable.map(row => { return { name: row.department, value: row.id } });
+    const answers = await inquirer.prompt(
+        {
+            type: "list",
+            message: "Select Department",
+            name: "departmentId",
+            choices: choices
+        }
+    );
+    const employeesTable = await queries.viewEmployeesByDepartment.runQuery(connection, answers.departmentId)
+    console.table(employeesTable);
+    setTimeout(() => init(), 500);
+}
+
+async function employeesByRole() {
+    const rolesTable = await queries.viewRoles.runQuery(connection);
+    console.log(rolesTable)
+    let choices = rolesTable.map(row => { return { name: row.title, value: row.id } });
+    const answers = await inquirer.prompt(
+        {
+            type: "list",
+            message: "Select Title",
+            name: "roleId",
+            choices: choices
+        }
+    );
+    const employeesTable = await queries.viewEmployeesByRole.runQuery(connection, answers.roleId)
+    console.table(employeesTable);
+    setTimeout(() => init(), 500);
+}
+
+async function addEmployee() {
+    const rolesTable = await queries.viewRoles.runQuery(connection);
+    let roleChoices = rolesTable.map(row => { return { name: row.title, value: row.id } });
+    const departmentTable = await queries.viewDepartments.runQuery(connection);
+    let departmentChoices = departmentTable.map(row => { return { name: row.department, value: row.id } });
+    const managerTable = await queries.viewEmployees.runQuery(connection);
+    let managerChoices = managerTable.map(row => { return { name: row.name, value: row.id } });
+    managerChoices.unshift({name: "none", value: null})
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            message: "First name",
+            name: "firstName",
+
+        },
+        {
+            type: "input",
+            message: "Last name",
+            name: "lastName"
+        },
+        {
+            type: "list",
+            message: "Title",
+            name: "titleId",
+            choices: roleChoices
+        },
+        {
+            type: "list",
+            message: "Manager",
+            name: "managerId",
+            choices: managerChoices
+        }
+    ]);
+    queries.addEmployee.runQuery(connection, [answers.firstName, answers.lastName, answers.titleId, answers.managerId]);
+    setTimeout(() => init(), 500);
+}
+
---
+++ js/queries.js
@@ -0,0 +1,74 @@
+// const mysql = require("mysql");
+// const connection = require("./connection")
+const { promisify } = require("util")
+
+
+class Query {
+    constructor(sqlString) {
+        this.sqlString = sqlString;
+    };
+    runQuery(connection, values) {
+        const that = this;
+        return new Promise(function (resolve, reject) {
+            connection.query(that.sqlString, values, (err, data) => {
+                if (err) {
+                    reject(new Error(err))
+                } else {
+                    resolve(data)
+                }
+            })
+        })
+        // return promiseQuery;
+    }
+}
+
+const viewEmployees = new Query(`SELECT employee.id AS id, 
+    CONCAT(employee.first_name, " ", employee.last_name) as name, 
+    title, 
+    name as department, 
+    salary, 
+    CONCAT(managers.first_name, " ", managers.last_name) AS manager
+    FROM department
+    INNER JOIN role ON department.id = role.department_id
+    INNER JOIN employee ON role.id = employee.role_id
+    LEFT JOIN employee managers ON managers.id = employee.manager_id;`)
+
+const viewDepartments = new Query('SELECT id, name AS department FROM department');
+
+const viewEmployeesByDepartment = new Query(`SELECT employee.id AS id, 
+    CONCAT(employee.first_name, " ", employee.last_name) as name, 
+    name as department, 
+    title, 
+    salary, 
+    CONCAT(managers.first_name, " ", managers.last_name) AS manager
+    FROM department
+    INNER JOIN role ON department.id = role.department_id
+    INNER JOIN employee ON role.id = employee.role_id
+    LEFT JOIN employee managers ON managers.id = employee.manager_id
+    WHERE department.id = ?;`);
+
+const viewRoles = new Query('SELECT id, title FROM role');
+
+const viewEmployeesByRole = new Query(`SELECT employee.id AS id, 
+    CONCAT(employee.first_name, " ", employee.last_name) as Name, 
+    name as department, 
+    title, 
+    salary, 
+    CONCAT(managers.first_name, " ", managers.last_name) AS manager
+    FROM department
+    INNER JOIN role ON department.id = role.department_id
+    INNER JOIN employee ON role.id = employee.role_id
+    LEFT JOIN employee managers ON managers.id = employee.manager_id
+    WHERE role.id = ?;`)
+
+const addEmployee = new Query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+    VALUES (?, ?, ?, ?);`)
+
+module.exports = {
+    viewEmployees: viewEmployees,
+    viewDepartments: viewDepartments,
+    viewEmployeesByDepartment: viewEmployeesByDepartment,
+    viewRoles: viewRoles,
+    viewEmployeesByRole: viewEmployeesByRole,
+    addEmployee: addEmployee
+}
